f334fe5368e7ef32ba9572ae58531771e7f57a23
곽민지 <mini0006@naver.com>
2024-01-06 21:49:22 +0900

    feat: Input 구현 중

--- lv-3/src/components/InputSection.jsx
+++ lv-3/src/components/InputSection.jsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SmallButton } from '../styles/CommonStyles';
+import { useState } from 'react';
 
 function InputSection() {
+  const [info, setInfo] = useState({
+    name: '',
+    price: '0',
+  });
+
+  const checkNumberHandler = (e) => {
+    if (
+      (e.keyCode > 47 && e.keyCode < 58) ||
+      e.keyCode === 8 || //backspace
+      e.keyCode === 37 ||
+      e.keyCode === 39 || //방향키 →, ←
+      e.keyCode === 46 //delete키
+    ) {
+      return;
+    } else {
+      e.preventDefault();
+    }
+  };
+
+  const addComma = (price) => {
+    let returnString = price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return returnString;
+  };
+
+  const nameChangeHandler = (e) => setInfo({ name: e.target.value, price: info.price });
+
+  const priceChangeHandler = (e) => {
+    let { value } = e.target;
+    let str = value[0] === '0' ? value.substr(1).replaceAll(',', '') : value.replaceAll(',', '');
+    setInfo({ name: info.name, price: str });
+  };
+
   return (
     <>
       <h1>Input</h1>
       <InputBoxContainer>
         <div>
           <label>이름</label>
-          <InputBox type="text" />
+          <InputBox type="text" onChange={nameChangeHandler} value={info.name} />
         </div>
         <div>
           <label>가격</label>
-          <InputBox type="text" value="0" />
+          <InputBox
+            type="text"
+            onKeyDown={checkNumberHandler}
+            onChange={priceChangeHandler}
+            value={addComma(info.price) || '0'}
+          />
         </div>
-        <SmallButton $primary>저장</SmallButton>
+        <SmallButton onClick={() => alert(`{name: ${info.name}, price: ${info.price}}`)} $primary>
+          저장
+        </SmallButton>
       </InputBoxContainer>
     </>
   );
